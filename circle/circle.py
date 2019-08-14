e217a19e4abaf480bd24ce6241878533653ab40e
kyad <53517611+kyad@users.noreply.github.com>
2019-08-14 13:42:50 +0900

    Change column order

--- circle/circle.py
+++ circle/circle.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 df = pd.read_csv(sys.argv[1])
 df.columns = ['enemy_x', 'enemy_y', 'enemy_qx', 'enemy_qy', 'enemy_qz', 'enemy_qw', 'enemy_ax', 'enemy_ay', 'enemy_az', 'my_x', 'my_y', 'my_qx', 'my_qy', 'my_qz', 'my_qw', 'my_ax', 'my_ay', 'my_az', 'circle_x', 'circle_y', 'circle_r'] # Fix my and enemy
+df = df[['my_x', 'my_y', 'my_qx', 'my_qy', 'my_qz', 'my_qw', 'my_ax', 'my_ay', 'my_az', 'enemy_x', 'enemy_y', 'enemy_qx', 'enemy_qy', 'enemy_qz', 'enemy_qw', 'enemy_ax', 'enemy_ay', 'enemy_az', 'circle_x', 'circle_y', 'circle_r']]
 df = df[(df['circle_x'] >= 0) & (df['circle_y'] < 300)]
 df['dx'] = df['enemy_x'] - df['my_x']
 df['dy'] = df['enemy_y'] - df['my_y']
