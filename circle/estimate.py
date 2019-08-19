71eea5a7bb696df3c31d9f32649d9865f0e1d1f9
kyad <53517611+kyad@users.noreply.github.com>
2019-08-19 16:23:37 +0900

    Add sin-theta estimation summary

--- circle/estimate.py
+++ circle/estimate.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 df = pd.read_csv(sys.argv[1])
 
-df['est_enemy_theta'] = -0.08390354 * df['circle_x'] \
-                    + 26.03749234111076
+df['est_enemy_sin_theta'] = -0.00143584 * df['circle_x'] \
+                    + 0.4458366274811388
+df['est_enemy_theta'] = np.rad2deg(np.arcsin(df['est_enemy_sin_theta']))
 df['est_enemy_v'] = 4.58779425e-09 * np.power(df['circle_y'], 4) \
                     - 1.14983273e-06 * np.power(df['circle_y'], 3) \
                     + 1.21335973e-04 * np.power(df['circle_y'], 2) \
@@ -25,5 +26,6 @@ df['diff_enemy_v'] = np.abs(df['est_enemy_v'] - df['v'])
 df['diff_enemy_theta'] = np.abs(df['est_enemy_theta'] - df['theta'])
 df['diff_enemy_x'] = np.abs(df['est_enemy_x'] - df['enemy_x'])
 df['diff_enemy_y'] = np.abs(df['est_enemy_y'] - df['enemy_y'])
+df['diff_enemy_sin_theta'] = np.abs(df['est_enemy_sin_theta'] - df['sin_theta'])
 
 df.to_csv(sys.stdout, float_format='%.6f')
